c9eb90ae3cc7fa3993076c2fba5d10d770f08929
Shagi <144171381+5hag1@users.noreply.github.com>
2023-09-09 21:40:21 -0500

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Jorge Cultid",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Profesional en ciberseguridad",
+    bio: "",
+    email: "shagi.5h4gi@gmail.com",
+    linkedin: "",
+    github: "https://github.com/5hag1",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Cyber-Sec`,
+      href: "https://github.com/5hag1",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Cyber-Sec",
+    description: "Blog dedicado a publicaciones de Ciberseguridad intentando aportar al conocimiento a partir de la experiencia",
   },
 
   // CONFIG configration (required)
@@ -47,7 +47,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
